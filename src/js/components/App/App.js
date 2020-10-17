52b5b0faccafaa48264278161fc0349dca4652c7
Yaser Somaf <yasersomaf@yasers-mbp.home>
2020-10-17 12:55:37 +0200

    Added start button

--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Car from '../Car/Car';
 import './App.css';
 
 const App = () => {
+	const [craSpeed, setCraSpeed] = useState();
+	const [showNewCarEvery, setShowNewCarEvery] = useState();
+	const [cars, setCars] = useState([]);
+	const [carPosition, setCarPosition] = useState(235);
+
+	const startHandler = () => {
+		setCars([]);
+		setCarPosition(235);
+	};
+
 	return <div className="game-container">
 		<div className="road">
-			<Car color="red" top={5} left={5} />
+			{cars.map(car => <Car color={car.color} top={car.top} left={car.left} />)}
+
+			<Car color="red" top={window.innerHeight - 110} left={carPosition} />
 		</div>
+		<button className="btn-stat" onClick={startHandler}>Start</button>
 	</div>;
 };
 
