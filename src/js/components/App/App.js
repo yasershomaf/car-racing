2634a5506b3f13e8dfb7c9ff640bc72bdb0eb999
Yaser Somaf <yasersomaf@yasers-mbp.home>
2020-10-17 13:25:43 +0200

    Added cars to the read

--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -1,24 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import Car from '../Car/Car';
 import './App.css';
 
 const App = () => {
 	const [craSpeed, setCraSpeed] = useState();
-	const [showNewCarEvery, setShowNewCarEvery] = useState();
+	const [crasSpeed, setCrasSpeed] = useState(10);
+	const [showNewCarEvery, setShowNewCarEvery] = useState(1000);
 	const [cars, setCars] = useState([]);
 	const [carPosition, setCarPosition] = useState(235);
+
+	const addCarIntervalRef = useRef(null);
 
 	const startHandler = () => {
 		setCars([]);
 		setCarPosition(235);
+
+		addCarIntervalRef.current = setInterval(() => {
+			setCars((cars) => [...cars, {
+				color: `rgb(${
+					Math.floor(Math.random() * 256)
+				}, ${
+					Math.floor(Math.random() * 256)
+				}, ${
+					Math.floor(Math.random() * 256)
+				})`,
+				left: Math.floor(Math.random() * 470),
+				top: -60
+			}]);
+		}, showNewCarEvery);
+
+		setInterval(() => {
+			setCars((cars) => cars.map(car => ({...car, top: car.top + crasSpeed})));
+		}, 50);
 	};
 
 	return <div className="game-container">
 		<div className="road">
-			{cars.map(car => <Car color={car.color} top={car.top} left={car.left} />)}
+			{cars.map((car, index) => <Car key={index} color={car.color} top={car.top} left={car.left} />)}
 
 			<Car color="red" top={window.innerHeight - 110} left={carPosition} />
 		</div>
+
 		<button className="btn-stat" onClick={startHandler}>Start</button>
 	</div>;
 };
