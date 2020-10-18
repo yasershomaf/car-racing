b4257713b90cf03f26d572a52f6b661418d2a3be
Yaser Somaf <yasersomaf@yasers-mbp.home>
2020-10-18 06:35:29 +0200

    Improved movement

--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -4,51 +4,52 @@ import './App.css';
 
 const App = () => {
 	const [craSpeed, setCraSpeed] = useState();
-	const [crasSpeed, setCrasSpeed] = useState(10);
+	const [crasSpeed, setCrasSpeed] = useState(15);
 	const [showNewCarEvery, setShowNewCarEvery] = useState(1000);
 	const [cars, setCars] = useState([]);
 	const [carPosition, setCarPosition] = useState(175);
 
 	const addCarIntervalRef = useRef(null);
 	const moveCarsIntervalRef = useRef(null);
+	const moveToLeft = useRef(false);
+	const moveToRight = useRef(false);
 
-	const moveCarHandler = useRef((e) => {
-		if (canMove.current) {
-			let newPosition;
-			if(e.keyCode === 37) {
-				setCarPosition(carPosition => {
-					newPosition = Math.max(0, carPosition - 10);
-					carPositionRef.current = newPosition;
-					return newPosition;
-				});
-			}
-			else if(e.keyCode === 39) {
-				setCarPosition(carPosition => {
-					newPosition = Math.min(350, carPosition + 10);
-					carPositionRef.current = newPosition;
-					return newPosition;
-				});
-			}
+	const keyDownHandler = useRef((e) => {
+		if(e.keyCode === 37) {
+			moveToLeft.current = true;
+		}
+		else if(e.keyCode === 39) {
+			moveToRight.current = true;
+		}
+	});
+
+	const keyUpHandler = useRef((e) => {
+		if(e.keyCode === 37) {
+			moveToLeft.current = false;
+		}
+		else if(e.keyCode === 39) {
+			moveToRight.current = false;
 		}
 	});
 
 	useEffect(() => {
-		document.addEventListener('keydown', moveCarHandler.current);
+		document.addEventListener('keydown', keyDownHandler.current);
+		document.addEventListener('keyup', keyUpHandler.current);
 		return () => {
-			document.removeEventListener('keydown', moveCarHandler.current);
+			document.removeEventListener('keydown', keyDownHandler.current);
+			document.removeEventListener('keyup', keyUpHandler.current);
 		}
 	}, []);
 
-	const canMove = useRef(false);
 	const carPositionRef = useRef([]);
 
 	const startHandler = () => {
 		setCars([]);
 		setCarPosition(175);
-		canMove.current = true;
 
 		addCarIntervalRef.current = setInterval(() => {
 			setCars((cars) => [...cars, {
+				id: new Date().getTime(),
 				color: `rgb(${
 					Math.floor(Math.random() * 256)
 				}, ${
@@ -62,6 +63,19 @@ const App = () => {
 		}, showNewCarEvery);
 
 		moveCarsIntervalRef.current = setInterval(() => {
+			if(moveToLeft.current) {
+				setCarPosition(carPosition => {
+					carPositionRef.current = Math.max(0, carPosition - 10);
+					return carPositionRef.current;
+				});
+			}
+			else if(moveToRight.current) {
+				setCarPosition(carPosition => {
+					carPositionRef.current = Math.min(350, carPosition + 10);
+					return carPositionRef.current;
+				});
+			}
+
 			setCars((cars) => {
 				const newCars = cars.map(car => {
 					const newTop = car.top + crasSpeed;
@@ -72,12 +86,11 @@ const App = () => {
 					) {
 						clearInterval(addCarIntervalRef.current);
 						clearInterval(moveCarsIntervalRef.current);
-						canMove.current = false;
 						console.log('you loose');
 					}
 
 					return {...car, top: car.top + crasSpeed}
-				});
+				}).filter(car => car.top < window.innerHeight);
 
 				return newCars;
 			});
@@ -86,7 +99,7 @@ const App = () => {
 
 	return <div className="game-container">
 		<div className="road">
-			{cars.map((car, index) => <Car key={index} color={car.color} top={car.top} left={car.left} />)}
+			{cars.map(car => <Car key={car.id} color={car.color} top={car.top} left={car.left} />)}
 
 			<Car color="red" top={window.innerHeight - 180} left={carPosition} />
 		</div>
--- src/js/components/Car/Car.js
+++ src/js/components/Car/Car.js
@@ -4,17 +4,17 @@ import './Car.css';
 const Car = ({color, top, left}) => {
   return (
     <div className="car" style={{top: top + 'px', left: left + 'px'}}>
-      <div class="car-body" style={{backgroundColor: color}}></div>
-      <div class="car-back" style={{borderTopColor: color}}></div>
-      <div class="front-glass"></div>
-      <div class="top-glass"></div>
-      <div class="back-glass"></div>
-      <div class="left-front-glass"></div>
-      <div class="left-glass"></div>
-      <div class="left-back-glass"></div>
-      <div class="right-front-glass"></div>
-      <div class="right-glass"></div>
-      <div class="right-back-glass"></div>
+      <div className="car-body" style={{backgroundColor: color}}></div>
+      <div className="car-back" style={{borderTopColor: color}}></div>
+      <div className="front-glass"></div>
+      <div className="top-glass"></div>
+      <div className="back-glass"></div>
+      <div className="left-front-glass"></div>
+      <div className="left-glass"></div>
+      <div className="left-back-glass"></div>
+      <div className="right-front-glass"></div>
+      <div className="right-glass"></div>
+      <div className="right-back-glass"></div>
     </div>
   );
 }
